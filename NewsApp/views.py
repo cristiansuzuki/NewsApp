3cc77b6b91f8ebdd63579e812388960f1800d4a7
cristiansuzuki <cristian.suzuki@foodzilla.com.br>
2021-05-15 18:31:24 -0300

    detalhes

--- NewsApp/views.py
+++ NewsApp/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from newsapi import NewsApiClient
 # Create your views here.
 
+# Função que faz requisição da API de acordo com os paramêtros passados
+
 
 def index(request):
     newsApi = NewsApiClient(api_key='17af1b67e52a44fa85a60b1f052df07d')
+
+    # Abaixo está o parametro get_top_headlines. Para pegar todos os dados de uma palavra
+    # pode-se usar get_everything e passar 'q=nomedapalavra' dentro da função
+
     headLines = newsApi.get_top_headlines(sources="bleacher-report")
     articles = headLines['articles']
     desc = []
