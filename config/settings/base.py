3415f52d6812c86e43533dc79226db8b2c9aa524
cristiansuzuki <cristian.suzuki@foodzilla.com.br>
2021-05-16 19:43:45 -0300

    heroku changes

--- config/settings/base.py
+++ config/settings/base.py
@@ -127,8 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
